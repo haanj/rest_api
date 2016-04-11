0ef40d60dc84e5085c37ec6cd7a39763ce777de7
haanj <joshua.haan@ymail.com>
2016-04-11 12:06:09 -0700

    delete and post work

--- haanj/build/bundle.js
+++ haanj/build/bundle.js
@@ -46,12 +46,12 @@
 
 	'use strict';
 	const angular = __webpack_require__(1);
-
 	const app = angular.module('SnackApp', []);
 
 	app.controller('SnackController', ['$http', function($http) {
 	  this.smokeTest = 'smoketest';
-	  this.snacks = ['snack1, snack2'];
+	  
+	  this.snacks = [];
 	  $http.get('http://localhost:3000/snacks')
 	    .then((result) => {
 	      console.log(result.data);
@@ -60,7 +60,50 @@
 	    function(err) {
 	      console.log(err); 
 	    });
+
+	  this.active = null;
+
+	  this.isActive = function(snack) {
+	    return snack == this.active;
+	  };
+
+	  this.makeActive = function(snack) {
+	    if (this.active == snack) return this.active = null;
+	    this.active = snack;
+	  };
+
+	  this.deleteSnack = function(snack) {
+	    $http.delete('http://localhost:3000/snacks/' + snack)
+	      .then(() => {
+	        this.snacks = this.snacks.filter((s)=>{
+	          return s._id != snack;
+	        });
+	      },
+	      function(err) {
+	        console.log(err); 
+	      });
+	  };
+
+	  this.addSnack = function(snackName, snackIngredients, tags) {
+	    console.log(snackName);
+	    console.log(snackIngredients);
+	    console.log(tags);
+	    let snack = {
+	      name: snackName,
+	      ingredients: snackIngredients.split(', '),
+	      tags: tags.split(', ')
+	    };
+	    $http.post('http://localhost:3000/snacks', snack) 
+	      .then((res) => {
+	        res.data
+	        this.snacks.push(res.data); 
+	      },
+	      function(err) {
+	        console.log(err);
+	      });
+	  };
 	}]);
+
 
 
 /***/ },
